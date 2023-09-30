188b20831f240b3cf192de7c0865794fa89f8ede
Simbirkinaa <simbirkinaaa@mail.ru>
2023-09-30 11:15:50 +0400

    Create tests

---
+++ Tests/unittest1.py
@@ -0,0 +1,40 @@
+import unittest
+import tkinter as tk
+from tkinter import Canvas
+from main import TriangleDrawer
+
+class TestTriangleDrawer(unittest.TestCase):
+
+    def setUp(self):
+        self.root = tk.Tk()
+        self.canvas = Canvas(self.root, width=300, height=300)
+        self.canvas.pack()
+        self.triangle_drawer = TriangleDrawer(self.canvas)
+
+    def test_initial_triangle_pattern(self):
+        # Проверка, что начальный образец треугольника отображается правильно
+        objects_on_canvas = self.canvas.find_all()
+        self.assertTrue(len(objects_on_canvas) > 0, "На холсте нет объектов")
+
+    def test_move_triangle(self):
+        # Проверка перемещения треугольника вправо
+        initial_x = self.triangle_drawer.x
+        event = tk.Event()
+        event.keysym = "Right"
+        self.triangle_drawer.move_triangle(event)
+        expected_x = initial_x + self.triangle_drawer.step
+        self.assertEqual(self.triangle_drawer.x, expected_x, "Координата x изменилась неправильно")
+
+    def test_change_pattern(self):
+        # Проверка изменения образца треугольника
+        initial_pattern_index = self.triangle_drawer.pattern_index
+        event = tk.Event()
+        self.triangle_drawer.change_pattern(event)
+        expected_pattern_index = (initial_pattern_index + 1) % len(self.triangle_drawer.triangle_patterns)
+        self.assertEqual(self.triangle_drawer.pattern_index, expected_pattern_index, "Индекс образца изменился неправильно")
+
+    def tearDown(self):
+        self.root.destroy()
+
+if __name__ == '__main__':
+    unittest.main()
--- main.py
+++ main.py
@@ -1,82 +1,75 @@
 import tkinter as tk
 
-# Глобальные переменные для хранения координат и образца треугольника
-x, y = 120, 150
-triangle_patterns = [
+class TriangleDrawer:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.x, self.y = 120, 150
+        self.step = 10
+        self.triangle_patterns = [
     [
-        (0, 0, 50, 0),   # основание снизу
-        (0, 0, 25, -50),  # левая сторона
-        (50, 0, 25, -50)  # правая сторона
-    ],
-[
-        (0, 0, 50, 0),   # основание вверху
-        (0, 0, -25, 50),  # левая сторона
-        (50, 0, -25, 50)  # правая сторона
+        (0, 0, 50, 0),
+        (0, 0, 25, -50),
+        (50, 0, 25, -50)
     ],
     [
-        (0, 0, 50, 0),   # основание
-        (0, 0, 25, 50),  # левая сторона
-        (50, 0, 25, 50)  # правая сторона
+        (0, 0, 50, 0),
+        (0, 0, -25, 50),
+        (50, 0, -25, 50)
     ],
     [
-        (0, 0, 50, 0),  # верхнее основание
-        (50, 0, 75, 50),   # правая сторона
-        (0, 0, 75, 50)  # левая сторона
+        (0, 0, 50, 0),
+        (0, 0, 25, 50),
+        (50, 0, 25, 50)
+    ],
+    [
+        (0, 0, 50, 0),
+        (50, 0, 75, 50),
+        (0, 0, 75, 50)
     ],
 ]
-pattern_index = 0
+        self.pattern_index = 0
+        self.draw_triangle()
 
-# Функция для отображения треугольника
-def draw_triangle():
-    canvas.delete("triangle")
-    for coords in triangle_patterns[pattern_index]:
-        canvas.create_line(
-            x + coords[0], y + coords[1], x + coords[2], y + coords[3],
-            fill="black", width=2, tags="triangle"
-        )
+    def draw_triangle(self):
+        self.canvas.delete("triangle")
+        for coords in self.triangle_patterns[self.pattern_index]:
+            self.canvas.create_line(
+                self.x + coords[0], self.y + coords[1], self.x + coords[2], self.y + coords[3],
+                fill="black", width=2, tags="triangle"
+            )
 
-# Функция для перемещения треугольника
-def move_triangle(event):
-    global x, y
-    step = 10  # Шаг перемещения
-    if event.keysym == "Right":
-        x += step
-    elif event.keysym == "Left":
-        x -= step
-    elif event.keysym == "Down":
-        y += step
-    elif event.keysym == "Up":
-        y -= step
-    draw_triangle()
+    def move_triangle(self, event):
+        step = 10  # Шаг перемещения
+        if event.keysym == "Right":
+            self.x += step
+        elif event.keysym == "Left":
+            self.x -= step
+        elif event.keysym == "Down":
+            self.y += step
+        elif event.keysym == "Up":
+            self.y -= step
+        self.draw_triangle()
 
-# Изменение образца треугольника
-def change_pattern(event):
-    global pattern_index
-    pattern_index = (pattern_index + 1) % len(triangle_patterns)
-    draw_triangle()
+    def change_pattern(self, event):
+        self.pattern_index = (self.pattern_index + 1) % len(self.triangle_patterns)
+        self.draw_triangle()
 
-# Выход из программы
-def exit_program(event):
-    if event.keysym == "Escape":
-        root.quit()
+    def exit_program(self, event):
+        if event.keysym == "Escape":
+            root.quit()
 
-# Создание основного окна
 root = tk.Tk()
 root.title("Управление треугольником")
-
-# Создание поля для отображения треугольника
 canvas = tk.Canvas(root, width=300, height=300)
 canvas.pack()
 
-# Назначение обработчиков событий клавиш корневому окну
-root.bind("<Right>", move_triangle)
-root.bind("<Left>", move_triangle)
-root.bind("<Down>", move_triangle)
-root.bind("<Up>", move_triangle)
-root.bind("<Return>", change_pattern)
-root.bind("<Escape>", exit_program)
+triangle_drawer = TriangleDrawer(canvas)
 
-# Отображение первого образца
-draw_triangle()
+root.bind("<Right>", triangle_drawer.move_triangle)
+root.bind("<Left>", triangle_drawer.move_triangle)
+root.bind("<Down>", triangle_drawer.move_triangle)
+root.bind("<Up>", triangle_drawer.move_triangle)
+root.bind("<Return>", triangle_drawer.change_pattern)
+root.bind("<Escape>", triangle_drawer.exit_program)
 
 root.mainloop()
